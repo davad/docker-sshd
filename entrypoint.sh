695b3e25d1cd4ecc16b34b67615674a76218dae2
David Landry <david@dmwl.net>
2018-12-06 10:39:30 -0500

    Unlock account, fix key import, add docker-compose file

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,16 +9,23 @@ sed -i "s/#PasswordAuthentication.*/PasswordAuthentication no/" /etc/ssh/sshd_co
 
 # Create accounts for users and fetch keys
 if [ "$USERS" ]; then
-  echo $USERS
-  for i in "$USERS"; do
-    echo $i
-    addgroup -S $i && adduser -S $i -G $i
-    HOME="/home/${i}"
-    if [ "$GIHUB_KEYS" ]; then
-      wget -O - "https://github.com/${i}.keys" >> "${HOME}/.ssh/authorized_keys"
-      cat $HOME/.ssh/authorized_keys
-    fi
-  done
+    echo $USERS
+    for i in "$USERS"; do
+        addgroup -S $i && adduser -S $i -G $i
+
+        # disable password access, unlock account, set shell
+        usermod -p '*' $i
+        passwd -u $i
+        usermod -s '/bin/bash' $i
+        tail /etc/passwd
+
+        HOME="/home/${i}"
+        if [ -n "$GITHUB" ]; then
+            echo "Import keys"
+            mkdir -p "${HOME}/.ssh"
+            wget -O - "https://github.com/${i}.keys" >> "${HOME}/.ssh/authorized_keys"
+        fi
+    done
 fi
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
